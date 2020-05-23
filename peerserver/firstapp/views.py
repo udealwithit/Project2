52adedadd5a0d5cfca014d4b74764f08e0e84097
udealwithit <udealwithit20@gmail.com>
2020-05-23 01:49:56 -0400

    Front end

--- peerserver/firstapp/views.py
+++ peerserver/firstapp/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 import socket
+from restserver.models import AirQData
+from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
 	context = {'peer_num': socket.gethostname()}
 	return render(request, "firstapp/index.html", context)
+
+def chart(request):
+	context = {
+		'time': [0],
+		'aqualities': [0],
+	}
+	return render(request, "firstapp/chart.html", context)
+
+def update_chart(request):
+	temp =	AirQData.objects.all()[:10]
+	time = []
+	aqualities = []
+
+	for i,dataset in enumerate(temp) :
+		time.append(i)
+		aqualities.append(dataset.NO2_Mean)
+
+	time = time[:10]
+	aqualities = aqualities[:10]
+	aqualities.reverse()
+
+	context = {
+		'time': time,
+		'aqualities': aqualities,
+	}
+	return JsonResponse(context)
